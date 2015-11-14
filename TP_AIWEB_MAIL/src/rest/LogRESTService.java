072d2cdc005f7146d2dc7441150863b682416618
marianfu <marianfurriel@gmail.com>
2015-11-14 18:23:52 -0300

    rest email

---
+++ TP_AIWEB_MAIL/src/rest/LogRESTService.java
@@ -0,0 +1,42 @@
+package rest;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.websocket.server.PathParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import bean.LogBean;
+
+
+
+@Stateless
+@Path("/log")
+public class LogRESTService {
+
+	@EJB
+	private LogBean logBean;
+
+	public LogRESTService() {
+
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/all")
+	public String getLogs() {
+		
+		return logBean.getLogs();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{modulo}")
+	public String getLogsPorModulo(@PathParam("modulo") String modulo) {
+		
+		return logBean.getLogsPorModulo(modulo);
+	}
+
+}
